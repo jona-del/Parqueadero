6853c70a369f09d04193591bba6ae6e02529cfac
Jonathan Hernandez <hernandezdelahozjonathan@gmail.com>
2026-05-17 15:52:05 -0500

    correcion de errores visualizador

--- visualizador.py
+++ visualizador.py
@@ -15,21 +15,21 @@ titulo = tk.Label(
 )
 titulo.pack(pady=10)
 
-resumen= tk.Label(
+resumen = tk.Label(
     ventana,
-    text="ocupadas: 0 | disponibles: 20",
-    font=("Arial", 18, "bold")
+    text="Ocupadas: 0 | Disponibles: 20",
+    font=("Arial", 12, "bold")
 )
-celdas_libres_label= tk.Label(
+resumen.pack(pady=5)
+
+celdas_libres_label = tk.Label(
     ventana,
-    text="Celdas Libres: 1, 2, 3, ..., 20",
+    text="Celdas libres: 1, 2, 3, ..., 20",
     font=("Arial", 10),
     wraplength=650,
     justify="center"
 )
-
 celdas_libres_label.pack(pady=5)
-
 
 tabla = ttk.Treeview(
     ventana,
@@ -39,7 +39,7 @@ tabla = ttk.Treeview(
 
 tabla.heading("placa", text="Placa")
 tabla.heading("hora", text="Hora Entrada")
-tabla.heading("celda", text="Celda")
+tabla.heading("celda", text="Celda Ocupada")
 
 tabla.pack(fill="both", expand=True, padx=20, pady=20)
 
@@ -48,8 +48,9 @@ def actualizar():
         tabla.delete(item)
 
     datos = parqueadero.obtenerDatosParqueadero()
-
     lineas = datos.strip().split("\n")
+
+    celdas_ocupadas = set()
 
     for linea in lineas:
         if linea.strip() != "":
@@ -62,17 +63,22 @@ def actualizar():
                     celdas_ocupadas.add(int(celda))
                 except ValueError:
                     pass
-celdas_libres=[] 
-for i in range(1, TOTAL_CELDAS + 1):
-    if i not in celdas_ocupadas:
-        celdas_libres.append(i)
-resumen.config(
-    text=f"Ocupadas: {len(celdas_ocupadas)} | Disponibles: {len(celdas_libres)}"
-)
-celdas_libres_label.config(
-    text="Celdas Libres:"+", ".join(map(str,celdas_libres))
-)
-ventana.after(1000, actualizar)
+
+    celdas_libres = []
+
+    for i in range(1, TOTAL_CELDAS + 1):
+        if i not in celdas_ocupadas:
+            celdas_libres.append(i)
+
+    resumen.config(
+        text=f"Ocupadas: {len(celdas_ocupadas)} | Disponibles: {len(celdas_libres)}"
+    )
+
+    celdas_libres_label.config(
+        text="Celdas libres: " + ", ".join(map(str, celdas_libres))
+    )
+
+    ventana.after(1000, actualizar)
 
 actualizar()
 ventana.mainloop()
